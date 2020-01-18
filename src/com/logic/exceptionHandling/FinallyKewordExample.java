046c85277aff115b6904c17ca99edfbf8f36a06f
forgyan <info.forgyan@gmail.com>
2020-01-18 20:42:48 +0530

    Exception Handling

---
+++ src/com/logic/exceptionHandling/FinallyKewordExample.java
@@ -0,0 +1,35 @@
+package com.logic.exceptionHandling;
+
+public class FinallyKewordExample {
+	
+	public static void withoutCatchBlock() {
+		try {
+			System.out.println("try block");
+		}finally {
+			System.out.println("catch block");
+		}
+	}
+	
+	public static void noExceptionThrown() {
+		try {
+			System.out.println("try block");
+		}catch (Exception e) {
+			System.out.println("catch block");
+		}finally {
+			System.out.println("finally block");
+		}
+	}
+	
+	public static void exceptionThrownNotHandled() {
+		try {
+			System.out.println("try block");
+			throw new Exception("exception occured");
+		}catch (Exception e) {
+			System.out.println("catch block");
+		}finally {
+			System.out.println("finally block");
+		}
+		
+	}
+
+}
