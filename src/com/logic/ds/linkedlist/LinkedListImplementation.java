529fcf0d04234b8f69c1ec2995ffe551716e5dd8
forgyan <info.forgyan@gmail.com>
2020-02-22 12:24:20 +0530

    Create own linkedlist using java

---
+++ src/com/logic/ds/linkedlist/LinkedListImplementation.java
@@ -0,0 +1,15 @@
+package com.logic.ds.linkedlist;
+
+public class LinkedListImplementation {
+
+	public static void main(String[] args) {
+		
+		LinkedList list = new LinkedList();
+		list.insert(18);
+		list.insert(45);
+		list.insert(78);
+		
+		list.show();
+	}
+
+}
---
+++ src/com/logic/ds/linkedlist/Node.java
@@ -0,0 +1,6 @@
+package com.logic.ds.linkedlist;
+
+public class Node {
+	int data;
+	Node next;
+}
